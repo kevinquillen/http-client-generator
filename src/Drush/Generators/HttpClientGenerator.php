638ebbb5c33fc451b5d2029ef9810afbc887f814
Kevin Quillen <kevinquillen@gmail.com>
2023-04-21 11:51:40 -0400

    Add interaction to collect endpoints from the user.

--- src/Drush/Generators/HttpClientGenerator.php
+++ src/Drush/Generators/HttpClientGenerator.php
@@ -5,6 +5,8 @@ declare(strict_types=1);
 namespace kevinquillen\Drush\Generators;
 
 use DrupalCodeGenerator\Command\ModuleGenerator;
+use Symfony\Component\Console\Question\Question;
+use DrupalCodeGenerator\Utils;
 
 /**
  * Implements HttpClient generator command.
@@ -41,6 +43,8 @@ final class HttpClientGenerator extends ModuleGenerator {
     $vars['package'] = '{machine_name|camelize}';
     $vars['menu_path'] = '{machine_name|u2h}';
     $vars['description'] = $this->ask('Module description', 'Generates an HTTP client for interacting with remote APIs.', '::validateRequired');
+    $vars['class_description'] = $this->ask('Class description', 'A simple interface for interacting with the remote HTTP API.', '::validateRequired');
+    $vars['endpoints'] = $this->collectEndpoints($vars);
 
     $this->addFile('{machine_name}.info.yml', 'module/model.info.yml')->skipIfExists();
     $this->addFile('config/schema/{machine_name}.schema.yml', 'config/schema/model.schema.yml')->appendIfExists();
@@ -50,6 +54,43 @@ final class HttpClientGenerator extends ModuleGenerator {
     $this->addFile('{machine_name}.links.task.yml', 'menu/task/model.links.task.yml')->appendIfExists();
     $this->addFile('{machine_name}.routing.yml', 'routing/model.routing.yml')->appendIfExists();
     $this->addFile('{machine_name}.services.yml', 'services/model.services.yml')->appendIfExists();
+  }
+
+  /**
+   * Collects endpoints from the user.
+   *
+   * @param array $vars
+   *   The input vars.
+   * @param bool $default
+   *   The users choice.
+   *
+   * @return array
+   */
+  protected function collectEndpoints(array &$vars, bool $default = TRUE) {
+    $vars['endpoints'] = [];
+
+    if (!$this->confirm('Would you like to add endpoints?', $default)) {
+      return $vars['endpoints'];
+    }
+
+    while (TRUE) {
+      $question = new Question('Enter the name of the endpoint (this will be a method name in the class)');
+      $endpoint = $this->io()->askQuestion($question);
+
+      if (!$endpoint) {
+        break;
+      }
+
+      $question = new Question('Enter the URI for the endpoint', $endpoint);
+      $uri = $this->io()->askQuestion($question);
+
+      $vars['endpoints'][] = [
+        'name' => $endpoint,
+        'uri' => $uri,
+      ];
+    }
+
+    return $vars['endpoints'];
   }
 
 }
